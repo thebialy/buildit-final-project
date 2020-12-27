ccd33c4303f344e0757c1c647086dd61dcd65e0e
thebialy <67696052+thebialy@users.noreply.github.com>
2020-12-26 18:43:20 -0600

    changed logout to push to index

--- buildit/src/components/layout.jsx
+++ buildit/src/components/layout.jsx
@@ -40,7 +40,7 @@ export function UserHeader() {
             <div 
                 onClick={() => {
                 auth.logout()
-                router.push("/login");
+                router.push("/");
             }} 
             className="cursor-pointer"
             > 
